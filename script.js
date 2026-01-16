775d4932e403157781dae850d87f56345ac0ecee
M-MernaTours <mostafasaeed9991@gmail.com>
2026-01-16 09:52:44 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+// ===== Automatic Slider =====
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+function showNextSlide() {
+  slides[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('active');
+}
+
+// Change slide every 4 seconds
+setInterval(showNextSlide, 4000);
